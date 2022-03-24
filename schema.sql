51fea0e0472291ec7c6851d3a83928b010ee04ab
Fatima Ezzahra elmenoun <felemenoun@gmail.com>
2022-03-24 18:54:27 +0100

    Create owners and specie table

--- schema.sql
+++ schema.sql
@@ -20,3 +20,27 @@ CREATE TABLE owners (
 	age INT,
   PRIMARY KEY (id)
 );
+
+/* Create owners table */
+CREATE TABLE owners (
+	id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+	full_name VARCHAR(400) NOT NULL,
+	age INT,
+  PRIMARY KEY (id)
+);
+
+/* Create species table */
+CREATE TABLE species (
+	id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(200) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+LTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners (id);
+
